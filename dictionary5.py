d5a7b0778458ad660dcaaf8eece38a2d4e545471
Toshio Kuratomi <a.badger@gmail.com>
2019-02-10 15:15:08 -0800

    Add a few more implementations and tests
    * Add chunking to the things we can test.  ie large set of data but the
      queue will only grow to chunksize before being emptied
    * Add a few more variants of dictionary4 that may or may not be faster.
      These have various tradeoffs as well

---
+++ dictionary5.py
@@ -0,0 +1,42 @@
+# Copyright: (c) 2019, Toshio Kuratomi <a.badger@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+import heapq
+from collections import defaultdict, deque
+
+class PriorityQueue:
+    def __init__(self):
+        self.ordered_store = []
+        self.lookup_store = defaultdict(deque)
+
+    def push(self, value, priority=None):
+        if priority is None:
+            priority = 0
+
+        if priority not in self.lookup_store:
+            heapq.heappush(self.ordered_store, priority)
+
+        self.lookup_store[priority].append(value)
+
+    def pop(self):
+        priority = self.ordered_store[0]
+        value = self.lookup_store[priority].popleft()
+
+        if not self.lookup_store[priority]:
+            heapq.heappop(self.ordered_store)
+
+        return value
+
+    def __len__(self):
+        total_len = 0
+        for queue in self.lookup_store.values():
+            total_len += len(queue)
+        return total_len
+
+    def __bool__(self):
+        return True if self.ordered_store else False
+
+    def compact(self):
+        """Reduce the memory requirements of the queue"""
+        for priority in list(self.lookup_store.keys()):
+            if not self.lookup_store[priority]:
+                del self.lookup_store[priority]
--- test_priority_queues.py
+++ test_priority_queues.py
@@ -5,6 +5,7 @@
 import random
 import timeit
 from collections import defaultdict
+from itertools import islice
 
 import pytest
 
@@ -12,14 +13,34 @@ import dictionary1
 import dictionary2
 import dictionary3
 import dictionary4
+import dictionary5
+import dictionary6
+import dictionary7
 import heapqdict
 import heapq1
 import priorityqueue
 
 
+# The highest priority to select from (also, the opposite of this is the lowest).
+# This is only the potential priorities.  Each entry is selected randomly from this list.
+# The more priorities, the less efficient the dictionary-based priority-queues
+PRIORITY_MAXIMA = 2**10
+
+# Number of entries into the queue
 LARGE_DATASET_LEN = 2**12
+
+# The number of entries that are added to the queue before the entire queue is processed
+# If LARGE_DATASET_LEN is larger than this, the queue will be emptied and refilled until
+# the entire dataset has been processed.
+CHUNKSIZE = LARGE_DATASET_LEN
+
+# Store some junk data in addition to the priority queue's record keeping to be a tad more realistic
 DATASIZE = 2**10
 DATA = 'a' * DATASIZE
+
+# Data to use when chcking for correctness.  This should include repeated priorities to check for
+# insertion order, None and 0 (which the queue should store at the same priority), positive and
+# negative priorities, and addition of priorities in both sorted and unsorted order.
 DATASET = (
            (80, DATA + '8'),
            (-10, DATA + '1'),
@@ -38,10 +59,15 @@ pytestmark = pytest.mark.usefixtures("create_large_dataset")
 
 @pytest.fixture(scope="module")
 def create_large_dataset():
+    """
+    Populate a large list of data to enter into the queue
+
+    The data has randomized priorities controlled by the constants defined above.
+    The data will only be generated once per pytest run so that all the tests in this module will
+    get the same dataset.
+    """
     global _large_dataset
-    # The more priorities, the less efficient the dictionary-based priority-queues
-    #priority_values = list(range(-2**10, 2**10))
-    priority_values = list(range(-2**2, 2**2))
+    priority_values = list(range(-PRIORITY_MAXIMA, PRIORITY_MAXIMA))
     _large_dataset = []
     for seq in range(0, LARGE_DATASET_LEN):
         priority = random.choice(priority_values)
@@ -52,8 +78,17 @@ def create_large_dataset():
 
 @pytest.fixture
 def timingfile():
+    """Log the timings of the tests to the file pq-timings.log"""
     with open('pq-timings.log', 'a') as f:
         yield f
+
+
+def chunk(dataset, chunksize):
+    """Return chunks of the data at a time"""
+    iterator = iter(dataset)
+    def _next_chunk():
+        return tuple(islice(iterator, chunksize))
+    return iter(_next_chunk, tuple())
 
 
 def check_priority_queue(QueueClass, dataset):
@@ -74,25 +109,26 @@ def check_priority_queue(QueueClass, dataset):
 
 def run_priority_queue(QueueClass, dataset):
     queue = QueueClass()
-    for priority, value in dataset:
-        if priority is None:
-            queue.push(value)
-        else:
-            queue.push(value, priority=priority)
+    for somedata in chunk(dataset, CHUNKSIZE):
+        for priority, value in somedata:
+            if priority is None:
+                queue.push(value)
+            else:
+                queue.push(value, priority=priority)
 
-    sorted_data = defaultdict(list)
-    prev_priority = -100000
-    while queue:
-        data = queue.pop()
-        dummy, priority, seq = data.split(':')
-        if priority == 'None':
-            priority = 0
-        priority = int(priority)
-        seq = int(seq)
-        assert priority >= prev_priority
-        prev_priority = priority
+        sorted_data = defaultdict(list)
+        prev_priority = -100000
+        while queue:
+            data = queue.pop()
+            dummy, priority, seq = data.split(':')
+            if priority == 'None':
+                priority = 0
+            priority = int(priority)
+            seq = int(seq)
+            assert priority >= prev_priority
+            prev_priority = priority
 
-        sorted_data[priority].append(seq)
+            sorted_data[priority].append(seq)
 
     for insertion_order in sorted_data.values():
         assert sorted(insertion_order) == insertion_order
@@ -105,6 +141,9 @@ def run_priority_queue(QueueClass, dataset):
                                         dictionary2.PriorityQueue,
                                         dictionary3.PriorityQueue,
                                         dictionary4.PriorityQueue,
+                                        dictionary5.PriorityQueue,
+                                        dictionary6.PriorityQueue,
+                                        dictionary7.PriorityQueue,
                                         ])
 def test_priority_queue_correctness(QueueClass, timingfile):
     import test_priority_queues
@@ -123,12 +162,15 @@ def test_priority_queue_correctness(QueueClass, timingfile):
                                         dictionary2.PriorityQueue,
                                         dictionary3.PriorityQueue,
                                         dictionary4.PriorityQueue,
+                                        dictionary5.PriorityQueue,
+                                        dictionary6.PriorityQueue,
+                                        dictionary7.PriorityQueue,
                                         ])
 def test_priority_queue_speed(QueueClass, timingfile):
     import test_priority_queues
     test_priority_queues.QueueClass = QueueClass
 
     timer = timeit.Timer('run_priority_queue(QueueClass, _large_dataset)', setup='from test_priority_queues import QueueClass, _large_dataset, run_priority_queue')
-    timing = timer.repeat(repeat=3, number=10)
+    timing = timer.repeat(repeat=3, number=100)
 
     timingfile.write(f'{QueueClass}: {timing}\n')
